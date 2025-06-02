a6a9bf5eefd6a26b076b99e99cedfaf0275a47e7
APEETEC <gabriel938@gmail.com>
2025-06-02 19:11:19 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+var mySvg = new Vivus('louise', {
+  type: 'delayed',
+  duration: 100,
+  // start: 'autostart'
+});
+var meter = new Vivus('lake', {
+  type: 'scenario',
+  duration: 80,
+  start: 'autostart'
+});
